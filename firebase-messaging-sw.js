a33c7cf9b9460080d146b6247ab796f608a42d8c
Manza Nazar <manzarnazar@gmail.com>
2025-12-18 19:20:10 +0000

    first commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "sadsa",
-    authDomain: "sas",
-    projectId: "ada",
-    storageBucket: "sd",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "sd"
+    apiKey: "AIzaSyCwE5GOeg4TUEsRt7wGOROakdcBm5lo24o",
+    authDomain: "bingo-bites.firebaseapp.com",
+    projectId: "bingo-bites",
+    storageBucket: "bingo-bites.firebasestorage.app",
+    messagingSenderId: "996869641828",
+    appId: "1:996869641828:web:701835d34e6caade537171",
+    measurementId: "G-15JLXZ8JS5"
 });
 
 const messaging = firebase.messaging();
